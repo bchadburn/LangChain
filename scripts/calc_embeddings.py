e74283bc1f3b10493edbe26be817a080bf2d6e8f
Brennen Chadburn <brennenchadburn@gmail.com>
2023-09-24 09:46:23 -0500

    Add files to ignore

---
+++ scripts/calc_embeddings.py
@@ -0,0 +1,49 @@
+from langchain.embeddings import OpenAIEmbeddings
+import os
+import sys
+from langchain.embeddings import LlamaCppEmbeddings
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from llama_index import SimpleDirectoryReader, VectorStoreIndex, LangchainEmbedding, ServiceContext
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+    
+from vectorstore_setup.vectorstore_functions import VectorStoreIndexManager
+
+documents = SimpleDirectoryReader('data/ex_docs').load_data()
+
+#### OpenAI Embeddings
+embed_model = OpenAIEmbeddings() # Specify OPENAI_API_KEY in .env file
+
+service_context = ServiceContext.from_defaults(embed_model=embed_model)
+vector_store_manager = VectorStoreIndexManager(documents, embedding_model=embed_model, service_context=service_context)
+vectorstore = vector_store_manager.vectorstore
+
+query_engine = vectorstore.as_query_engine(
+    verbose=True, 
+)
+
+query_text = "What is something we know about Paul Graham"
+response = query_engine.query(query_text)
+print(response)
+
+#### Llama model
+
+embed_model = LangchainEmbedding(
+    HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+)
+
+service_context = ServiceContext.from_defaults(embed_model=embed_model)
+vector_store_manager = VectorStoreIndexManager(documents, embedding_model=embed_model, service_context=service_context)
+vectorstore = vector_store_manager.vectorstore
+
+query_engine = vectorstore.as_query_engine(
+    verbose=True, 
+)
+
+query_text = "What is something we know about Paul Graham"
+response = query_engine.query(query_text)
+print(response)
+
--- vectorstore_setup/vectorstore_functions.py
+++ vectorstore_setup/vectorstore_functions.py
@@ -1,16 +1,37 @@
-
 from langchain.vectorstores import Chroma
+from llama_index import VectorStoreIndex, ServiceContext
+from chromadb.errors import InvalidDimensionException
 
-class ChromaVectorStoreManager:
+class VectorStoreManager:
     def __init__(self, documents, embedding_model):
         self.documents = documents
         self.embedding_model = embedding_model
         self.vectorstore = None
+    
+    def create_vector_store(self):
+        # To be implemented in subclasses
+        pass
 
+class ChromaVectorStoreManager(VectorStoreManager):  
+    def __init__(self, documents, embedding_model):
+        super().__init__(documents, embedding_model)
+        self.create_vector_store()
+        
     def create_vector_store(self):
         try:
             self.vectorstore = Chroma.from_documents(documents=self.documents, embedding=self.embedding_model)
         except InvalidDimensionException:
             Chroma().delete_collection()
             self.vectorstore = Chroma.from_documents(documents=self.documents, embedding=self.embedding_model)
-    
+
+class VectorStoreIndexManager(VectorStoreManager):
+    def __init__(self, documents, embedding_model, service_context):
+        super().__init__(documents, embedding_model)
+        self.service_context = service_context
+        self.create_vector_store()
+        
+    def create_vector_store(self):
+        self.vectorstore = VectorStoreIndex.from_documents(
+        self.documents, 
+        service_context=self.service_context,
+    )
